77b289ddb7add011e9e0b14002151173f1f75c67
zakwarren <41547855+PoignantWizard@users.noreply.github.com>
2019-08-17 15:11:55 +0100

    basic list

---
+++ listitems.js
@@ -0,0 +1,17 @@
+const kitList = [
+    {
+        "category": "camping",
+        "kit": [
+            "tent",
+            "sleeping bag",
+            "roll mat"
+        ]
+    },
+    {
+        "category": "boating",
+        "kit": [
+            "towel",
+            "waterproofs"
+        ]
+    }
+]
