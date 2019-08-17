bbf216e41e3b92079d199b17aaf1dbea2acdb150
zakwarren <41547855+PoignantWizard@users.noreply.github.com>
2019-08-17 16:18:34 +0100

    expanded kit list

--- listitems.js
+++ listitems.js
@@ -1,17 +1,53 @@
 const kitList = [
     {
-        "category": "camping",
+        "category": "clothing",
         "kit": [
-            "tent",
-            "sleeping bag",
-            "roll mat"
+            "t-shirts",
+            "trousers",
+            "warm tops",
+            "underwear",
+            "socks",
+            "warm sleeping clothes",
+            "sun hat",
+            "warm hat",
+            "waterproof coat and trousers",
+            "walking boots",
         ]
     },
     {
         "category": "boating",
         "kit": [
+            "non-cotton top / rash vest",
+            "shorts",
+            "old trainers / wet shoes",
+            "waterproofs",
             "towel",
-            "waterproofs"
+            "dry bag",
+            "buoyancy aid"
+        ]
+    },
+    {
+        "category": "camping",
+        "kit": [
+            "tent",
+            "sleeping bag",
+            "blanket",
+            "roll mat",
+            "pillow",
+            "torch",
+            "wash kit",
+            "sun cream",
+            "insect repellent",
+            "water bottle",
+            "plate, bowl, cutlery, mug",
+            "pen knife"
+        ]
+    },
+    {
+        "category": "organisation",
+        "kit": [
+            "uniform",
+            "paperwork"
         ]
     }
 ]
